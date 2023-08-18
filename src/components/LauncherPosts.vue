b4b99056b0318270d9080b9f61f9f15038377b1f
HoraceHyy <horacehuang17@gmail.com>
2023-08-18 16:30:50 +0800

    fix. minor optimization to posts size

--- src/components/LauncherPosts.vue
+++ src/components/LauncherPosts.vue
@@ -14,7 +14,7 @@ const openLink = (url) => {
 <template>
     <el-tabs v-model="tabsModel">
         <el-tab-pane label="资讯" name="aaa">
-            <el-scrollbar class="w-full h-full" max-height="64px">
+            <el-scrollbar class="w-full h-full" max-height="62px">
                 <div class="h-max pr-2 pb-2">
                     <div v-for="post in postTypeMap.get('资讯')"
                         class="w-full justify-between flex flex-row p-1 pl-2 hover:bg-white rounded hover:shadow-md transition-all cursor-pointer"
@@ -27,7 +27,7 @@ const openLink = (url) => {
             </el-scrollbar>
         </el-tab-pane>
         <el-tab-pane label="活动" name="bbb">
-            <el-scrollbar class="w-full h-full" max-height="64px">
+            <el-scrollbar class="w-full h-full" max-height="62px">
                 <div class="h-max pr-2 pb-2">
                     <div v-for="post in postTypeMap.get('活动')"
                         class="w-full justify-between flex flex-row p-1 pl-2 hover:bg-white rounded hover:shadow-md transition-all cursor-pointer"
@@ -40,7 +40,7 @@ const openLink = (url) => {
             </el-scrollbar>
         </el-tab-pane>
         <el-tab-pane label="公告" name="ccc">
-            <el-scrollbar class="w-full h-full" max-height="64px">
+            <el-scrollbar class="w-full h-full" max-height="62px">
                 <div class="h-max pr-2 pb-2">
                     <div v-for="post in postTypeMap.get('公告')"
                         class="w-full justify-between flex flex-row p-1 pl-2 hover:bg-white rounded hover:shadow-md transition-all cursor-pointer"
