21dc2a8ad23cf1e842103697353787ac7df8e1ef
HoraceHyy <horacehuang17@gmail.com>
2023-11-19 14:28:36 +0800

    feat: French translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,9 +4,10 @@ import enUS from './en_US.json'
 import zhTW from './zh_TW.json'
 import ruRU from './ru_RU.json'
 import jaJP from './ja_JP.json'
+import frFR from './fr_FR.json'
 
 type MessageSchema = typeof enUS
-type lang = 'en_US' | 'zh_CN' | 'zh_TW' | 'ru_RU' | 'ja_JP'
+type lang = 'en_US' | 'zh_CN' | 'zh_TW' | 'ru_RU' | 'ja_JP' | 'fr_FR'
 
 const i18n = createI18n<[MessageSchema], lang>({
   // default locale
@@ -20,15 +21,15 @@ const i18n = createI18n<[MessageSchema], lang>({
     'zh_CN': zhCN,
     'zh_TW': zhTW,
     'ru_RU': ruRU,
-    'ja_JP': jaJP
-
+    'ja_JP': jaJP,
+    'fr_FR': frFR
   },
 })
 
 export default i18n
 
-export const availableLangCodes = ['en_US', 'zh_CN', 'zh_TW', 'ru_RU', 'ja_JP']
-export const availableLangNames = ['English', '简体中文', '繁體中文', 'Pусский', '日本語']
+export const availableLangCodes = ['en_US', 'zh_CN', 'zh_TW', 'ru_RU', 'ja_JP', 'fr_FR']
+export const availableLangNames = ['English', '简体中文', '繁體中文', 'Pусский', '日本語', 'Français']
 
 export const currentLocale = () => {
   return i18n.global.locale
